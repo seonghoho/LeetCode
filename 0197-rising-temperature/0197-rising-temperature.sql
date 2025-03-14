247a218318debb2bf686c3abde25b7af52d5e08f
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2025-03-14 12:52:54 +0900

    Time: 570 ms (86.28%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,10 @@
 SELECT
     a.id
 FROM
-    Weather a 
-    INNER JOIN Weather b
-    ON DATEDIFF(a.recordDate, b.recordDate) = 1 AND
-    a.temperature > b.temperature
+    Weather a
+    INNER JOIN
+        Weather b
+    ON
+        DATEDIFF(a.recordDate, b.recordDate) = 1
+    AND
+        a.temperature > b.temperature
