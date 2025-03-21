eb7f94068cc853f5ef1e867e0c63736ef18be1b7
Choi Seongho <128336950+seonghoho@users.noreply.github.com>
2025-03-21 12:52:27 +0900

    Time: 1106 ms (95.07%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,18 +1,16 @@
 SELECT
     s.user_id,
-    COALESCE(ROUND(c.confirm_cnt / c.total_cnt, 2), 0) AS confirmation_rate
+    COALESCE(ROUND(
+        COUNT(CASE 
+            WHEN c.action LIKE 'confirmed'
+            THEN 1
+            END) 
+        / 
+        COUNT(c.user_id), 2), 0) AS confirmation_rate
 FROM
     Signups s
     LEFT JOIN
-    (SELECT
-        user_id,
-        COUNT(CASE 
-            WHEN action LIKE 'confirmed'
-            THEN 1
-            END) AS confirm_cnt,
-        COUNT(user_id) AS total_cnt
-    FROM
-        Confirmations
-    GROUP BY
-        user_id) c
+    Confirmations c
     ON s.user_id = c.user_id
+GROUP BY
+        s.user_id
